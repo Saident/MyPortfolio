74bce5c473f0cb52c026b25e2786081d608ed475
Saident <averilprimayuda@gmail.com>
2026-04-02 13:11:22 +0700

    added white bg globally

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Contact from './components/Contact'
 
 function App() {
   return (
-    <div>
+    <div className="w-full bg-white min-h-screen"> 
       <Navbar />
       <Hero />
       <About />
