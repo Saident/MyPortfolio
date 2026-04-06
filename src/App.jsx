a8a38101bffb422c4395794a3344d37eada01488
Saident <averilprimayuda@gmail.com>
2026-04-06 14:34:47 +0700

    update background and removed line

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,16 @@ import About from './components/About'
 import Skills from './components/Skills'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
-import CustomCursor from './components/CustomCursor'
 
 function App() {
   return (
-    <div className="w-full bg-white min-h-screen relative">
-      <div className="relative z-10">
-        <Navbar />
-        <Hero />
-        
-        <hr className="border-t border-black opacity-10" />
-        <About />
-
-        <hr className="border-t border-black opacity-10" />
-        <Skills />
-
-        <hr className="border-t border-black opacity-10" />
-        <Projects />
-
-        <hr className="border-t border-black opacity-10" />
-        <Contact />
-      </div>
+    <div className="w-full bg-[#fafaf8] min-h-screen">
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
     </div>
   )
 }
